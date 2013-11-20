3dccac6b6e686f3777108f38854cf1c8fc5b1e93
Lavoisier Cornerstone <vois.cornerstone@gmail.com>
2013-11-19 23:30:16 -0500

    completed but never pushed

---
+++ is_between/is_between.rb
@@ -0,0 +1,18 @@
+#Demonstrate that 15 is between 12 and 18 by creating a method called
+#`is_between?` in the `Integer` class that returns true is the number is between
+# a max and min value that are given as arguments
+
+#To test your solution run `rspec is_between_spec.rb` in your terminal
+
+class Integer
+
+  def is_between?(min,max)
+      if self > min && self < max
+        true
+     else
+        false
+    end
+
+  end
+
+end
